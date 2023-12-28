e63459e893149795ca6f4b7334cd74681bad2215
Timur Turatbekov <timatur242005@gmail.com>
2023-12-28 19:52:24 +0300

    Level text

--- scripts/ui/branch.gd
+++ scripts/ui/branch.gd
@@ -6,16 +6,20 @@ extends TextureRect
 
 var _tween: Tween;
 
+@onready var _underline = $LevelBox/Underline;
+
 
 func _on_mouse_entered():
 	if _tween:
 		_tween.kill();
-	_tween = create_tween();
+	_tween = create_tween().set_parallel(true);
 	_tween.tween_property(self, "scale", Vector2.ONE * hovered_scale, trans_duration).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD);
+	_tween.tween_property(_underline, "value", 1.0, trans_duration).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD);
 
 
 func _on_mouse_exited():
 	if _tween:
 		_tween.kill();
-	_tween = create_tween();
+	_tween = create_tween().set_parallel(true);
 	_tween.tween_property(self, "scale", Vector2.ONE * normal_scale, trans_duration).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD);
+	_tween.tween_property(_underline, "value", 0.0, trans_duration).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD);
