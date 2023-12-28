90f55e4c37d2ff422eafd55e80baf8cc34ba8baa
Timur Turatbekov <timatur242005@gmail.com>
2023-12-28 20:32:53 +0300

    Finalized four level selector

--- scripts/ui/branch.gd
+++ scripts/ui/branch.gd
@@ -1,5 +1,7 @@
+@tool
 extends TextureRect
 
+@export_enum("CORNER_TOP_LEFT", "CORNER_TOP_RIGHT", "CORNER_BOTTOM_RIGHT", "CORNER_BOTTOM_LEFT") var corner: int;
 @export var normal_scale = 1.0;
 @export var hovered_scale = 1.075;
 @export var trans_duration = 0.2;
@@ -7,9 +9,24 @@ extends TextureRect
 var _tween: Tween;
 
 @onready var _underline = $LevelBox/Underline;
+@onready var _level_box = $LevelBox
+
+
+func _ready():
+	match corner:
+		CORNER_TOP_LEFT:
+			_level_box.position = $TopLeft.position;
+		CORNER_TOP_RIGHT: 
+			_level_box.position = $TopRight.position;
+		CORNER_BOTTOM_RIGHT:
+			_level_box.position = $BottomRight.position;
+		CORNER_BOTTOM_LEFT:
+			_level_box.position = $BottomLeft.position;
 
 
 func _on_mouse_entered():
+	if Engine.is_editor_hint():
+		return;
 	if _tween:
 		_tween.kill();
 	_tween = create_tween().set_parallel(true);
@@ -18,6 +35,8 @@ func _on_mouse_entered():
 
 
 func _on_mouse_exited():
+	if Engine.is_editor_hint():
+		return;
 	if _tween:
 		_tween.kill();
 	_tween = create_tween().set_parallel(true);
