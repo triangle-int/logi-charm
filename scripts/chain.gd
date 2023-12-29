3a6e5a0c9657ec3384f72fe7f9ffd734c703b55f
boris1592 <buzin.boria@gmail.com>
2023-12-29 22:29:37 +0300

    Implemented chains

---
+++ scripts/chain.gd
@@ -0,0 +1,21 @@
+@tool
+
+extends Node2D
+
+@export var chain_node: PackedScene
+@export var node_length: float
+@export var radius: float
+
+func _ready():
+	var node_count = floor(radius * TAU / node_length) as int
+	
+	if node_count % 2 == 1:
+		node_count += 1
+
+	for index in range(node_count):
+		var angle = (index as float) / node_count * TAU
+		var node = chain_node.instantiate() as Sprite2D
+		node.position = Vector2(cos(angle), -sin(angle)) * radius
+		node.rotation = -angle
+		node.frame = index % 2
+		add_child(node)
