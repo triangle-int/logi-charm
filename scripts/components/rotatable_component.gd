2848591a6fa032d1184f6fc8650213796d7d010f
boris1592 <buzin.boria@gmail.com>
2023-12-29 20:28:24 +0300

    Implemented memory cells

--- scripts/components/rotatable_component.gd
+++ scripts/components/rotatable_component.gd
@@ -7,6 +7,6 @@ extends BaseComponent
 func _ready():
 	super()
 
-	var index: int = floor(((angle + PI / 4) / TAU) * 4)
+	var index = floor(((angle + PI / 4) / TAU) * 4) as int
 	var frame = {0: 0, 1: 3, 2: 1, 3: 2}[index]
 	sprite_2d.frame = frame
