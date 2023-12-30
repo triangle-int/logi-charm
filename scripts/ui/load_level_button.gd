4cc220ee0ba7a64a318ba3f30870678476101860
boris1592 <buzin.boria@gmail.com>
2023-12-30 16:51:49 +0300

    Implemented level unlocking, fixed imports for other fonts

--- scripts/ui/load_level_button.gd
+++ scripts/ui/load_level_button.gd
@@ -1,6 +1,12 @@
-extends Node
+extends Button
 
 @export var level_index: int
+
+@onready var label = $Label
+
+func _ready():
+	disabled = level_index != 0 and not LevelProgressManager.is_level_beaten(level_index - 1)
+	label.text = str(level_index).lpad(2, '0')
 
 func _on_pressed():
 	LoadManager.load_done.connect(_on_game_loaded)
